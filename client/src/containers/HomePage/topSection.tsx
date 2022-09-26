6bb5c7c592617ed91112b9b18a7c72aa2a6022fa
justinhwang92 <justinhwang92@gmail.com>
2022-09-26 13:26:45 -0400

    Add button component

--- client/src/containers/HomePage/topSection.tsx
+++ client/src/containers/HomePage/topSection.tsx
@@ -4,6 +4,7 @@ import tw from 'twin.macro';
 import MclarenCarImg from '../../assets/images/mclaren-orange-big.png';
 import BlobImg from '../../assets/images/blob.svg';
 import { SCREENS } from '../../components/responsive';
+import { Button } from '../../components/button';
 
 const TopSectionContainer = styled.div`
   min-height: 400px;
@@ -141,6 +142,14 @@ const StandaloneCar = styled.div`
   }
 `;
 
+const ButtonsContainer = styled.div`
+  ${tw`
+    flex
+    flex-wrap
+    mt-4
+  `}
+`;
+
 export function TopSection() {
   return (
     <TopSectionContainer>
@@ -151,6 +160,10 @@ export function TopSection() {
           at the best price for you and get the best quality cars for as long as
           you like
         </Description>
+        <ButtonsContainer>
+          <Button theme="outlined" text="Book Your Ride" />
+          <Button theme="filled" text="Sell Your Car" />
+        </ButtonsContainer>
       </LeftContainer>
       <RightContainer>
         <BlobContainer>
