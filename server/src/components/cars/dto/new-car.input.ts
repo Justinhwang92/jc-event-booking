882a3b2f6ef6bb76119b325ddd0ca7f4e7fb41a8
justinhwang92 <justinhwang92@gmail.com>
2022-09-29 14:23:36 -0400

    Add vaildation for addNewCar

--- server/src/components/cars/dto/new-car.input.ts
+++ server/src/components/cars/dto/new-car.input.ts
@@ -1,14 +1,19 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { Max, Min } from 'class-validator';
 
 @InputType()
 export class NewCarInput {
   @Field()
   name: string;
 
-  @Field((type) => Number)
+  @Field((type) => Int)
+  @Max(1000, { message: 'Daily price must be less than $1000' })
+  @Min(10, { message: 'Daily price must be greater than $10' })
   dailyPrice: number;
 
-  @Field((type) => Number)
+  @Field((type) => Int)
+  @Max(20000, { message: 'Monthly price must be less than $20000' })
+  @Min(1500, { message: 'Monthly price must be greater than $1500' })
   monthlyPrice: number;
 
   @Field()
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new BaseExceptionFilter());
+
+  // for validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
 
   await app.listen(process.env.PORT || 9000);
 }
